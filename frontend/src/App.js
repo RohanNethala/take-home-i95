44c2f36c27c938704add21957833d73de3ce5e8d
RohanNethala <afortyinchvertical@gmail.com>
2025-05-03 12:02:51 -0400

    Created frontend title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.css'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Product Recommendation System</h1>
       </header>
+      <main>
+        <p>Loading...</p>
+      </main>
     </div>
   );
 }
